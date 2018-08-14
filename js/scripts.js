4daa22f7e64ffbd7a6e9a11512fdc8c5ca95f00b
Brian Nelson and David Zhu <david@>
2018-08-14 15:53:30 -0700

    add prime number filter

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,24 @@
-var primeArray = [2];
 
-var isPrime = function(inputNumber) {
-  var numArray = [];
-  //make first array of all potential prime numbers
-  for (i=2;i<inputNumber;i++) {
-    numArray.push(i);
+
+var prime = function(num) {
+  if (num < 2) return [];
+  else {
+    var result = [];
+    for (var i = num; i>=2; i--) {
+      result.push(i);
+    }
+    for (var prime=2; prime<=num;prime++) {
+      result = result.filter(function(number) {
+        return (number % prime !== 0 || number === prime);
+      });
+    }
+  return result;
   }
-  var updatedArray = [];
-  var loop = numArray.length;
-  //remove multiples of the first prime number
-  for(x=0;updatedArray.length <= 1;x++) {
+}
 
-    for (i=0;i<=loop;i++) {
-      if (numArray[i] % primeArray[x] !== 0) {
-        updatedArray.push(numArray[i]);
-      }
-    };
-    // alert("e" + even + " o " + odd);
-    // alert(numArray + " . " + updatedArray);
-    loop = updatedArray.length;
-    return updatedArray;
-    primeArray.push(updatedArray[0]);
-  };
-};
 
-// updatedArray.splice(0);
+
+
 
 
 
@@ -34,8 +28,9 @@ $(document).ready(function() {
   $("#prime-method").submit(function(event) {
 
     var userInput = $("#numInput").val();
-    var output = isPrime(userInput);
+    var output = prime(userInput);
     alert(output);
+    // alert(output + " /////" + updatedArray);
     event.preventDefault();
   });
 });
